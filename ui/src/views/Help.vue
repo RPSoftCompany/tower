18107e70b574a92612ee745325c1a5d90ef8347b
Radek <radekprzewuski@gmail.com>
2020-10-04 08:18:56 +0200

    cicd udpate

---
+++ ui/src/views/Help.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="text-center text-h5">
+      Tower Configuration Server Documentation
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Help',
+    // mounted () {
+    //   const helpName = this.$route.params.name
+    //   console.log(helpName)
+    // },
+  }
+</script>
