d963773122fca5668b890565ebafb9c999490fc0
Radoslaw Przewuski <radekprzewuski@gmail.com>
2025-02-05 12:42:57 +0100

    invalid passwords fix and audit updates

--- backend/src/modules/max-constant-variable/max-constant-variable.schema.ts
+++ backend/src/modules/max-constant-variable/max-constant-variable.schema.ts
@@ -1,5 +1,8 @@
 import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { ConstantVariable } from '../constant-variables/constant-variables.schema';
+import {ConstantVariable, ConstantVariableObject} from '../constant-variables/constant-variables.schema';
+import {ConfigurationVariable} from "../configurations/configuration.schema";
+import {decryptPassword} from "../../helpers/encryptionHelper";
+import {maxConfiguration, MaxConfigurationSchema} from "../max-configuration/max-configuration.schema";
 
 @Schema({
   collection: 'maxConstantVariable',
@@ -9,5 +12,39 @@ export class MaxConstantVariable extends ConstantVariable {}
 
 const MaxConstantVariableSchema =
   SchemaFactory.createForClass(MaxConstantVariable);
+
+MaxConstantVariableSchema.post('find', (docs: MaxConstantVariable[]) => {
+  docs = docs.map((doc: MaxConstantVariable) => {
+    if (doc.variables) {
+      doc.variables = doc.variables.map((variable: ConstantVariableObject) => {
+        if (variable.type === 'password') {
+          variable.value = decryptPassword(variable.value);
+        }
+
+        return variable;
+      });
+    }
+
+    return doc;
+  });
+
+  return docs;
+});
+
+MaxConstantVariableSchema.post('aggregate', (docs: MaxConstantVariable[]) => {
+  if (Array.isArray(docs)) {
+    for (const doc of docs) {
+      if (doc.variables) {
+        doc.variables = doc.variables.map((variable: ConstantVariableObject) => {
+          if (variable.type === 'password') {
+            variable.value = decryptPassword(variable.value);
+          }
+
+          return variable;
+        });
+      }
+    }
+  }
+});
 
 export { MaxConstantVariableSchema };
